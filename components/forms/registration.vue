c824c3df5b96aca1ac4c670f81c1c31f286e0896
LeeMing-Bou <pozzzming123@gmail.com>
2025-02-01 13:51:36 +0700

    Testing

--- components/forms/registration.vue
+++ components/forms/registration.vue
@@ -12,7 +12,8 @@
       md:mt-0
     " method="post" @submit.prevent="submit">
     <h2 class="text-gray-900 text-lg font-medium title-font mb-5">
-      {{ $t("sign_up") }}
+      <!-- {{ $t("sign_up") }} -->
+        SIGN UP
     </h2>
     <div class="relative mb-4">
       <label for="name" class="leading-7 text-sm text-gray-600">{{
